5f23e75884a7aa357a7957e2a000e539552521a9
pijanydrwal <pijanydrwal@gmail.com>
2026-03-25 10:07:27 +0100

    Update SettingsDialog.h

--- src/SettingsDialog.h
+++ src/SettingsDialog.h
@@ -1,24 +1,51 @@
-// SettingsDialog.h
 #ifndef SETTINGS_DIALOG_H
 #define SETTINGS_DIALOG_H
 
-#include <QDialog>
-#include <QString>
+#include <windows.h>
+#include <string>
+#include "ImapClient.h"
 
-class SettingsDialog : public QDialog {
-    Q_OBJECT
-
+class SettingsDialog {
 public:
-    SettingsDialog(QWidget *parent = nullptr);
+    SettingsDialog(HINSTANCE hInst, HWND hParent);
     ~SettingsDialog();
 
-private:
-    void setupUi();
-    void loadSettings();
-    void saveSettings();
+    // Show modal dialog, returns true if OK was clicked
+    bool show();
 
-private slots:
-    void on_applyButton_clicked();
+    // Get/set current config
+    ImapConfig getConfig() const { return m_config; }
+    void setConfig(const ImapConfig& cfg) { m_config = cfg; }
+
+    // Save/load from INI file next to plugin DLL
+    bool saveConfig(const std::string& iniPath);
+    bool loadConfig(const std::string& iniPath);
+
+private:
+    HINSTANCE m_hInst;
+    HWND m_hParent;
+    HWND m_hDlg;
+    ImapConfig m_config;
+
+    static INT_PTR CALLBACK dlgProc(HWND hDlg, UINT msg,
+                                     WPARAM wParam, LPARAM lParam);
+    INT_PTR handleMessage(HWND hDlg, UINT msg, WPARAM wParam, LPARAM lParam);
+    void populateFields(HWND hDlg);
+    void readFields(HWND hDlg);
+    void autoDetectServer(HWND hDlg);
+
+    // Control IDs
+    enum {
+        IDC_SERVER  = 101,
+        IDC_PORT    = 102,
+        IDC_USER    = 103,
+        IDC_PASS    = 104,
+        IDC_FOLDER  = 105,
+        IDC_SSL     = 106,
+        IDC_TEST    = 107,
+        IDC_STATUS  = 108,
+        IDC_AUTODET = 109
+    };
 };
 
 #endif // SETTINGS_DIALOG_H
